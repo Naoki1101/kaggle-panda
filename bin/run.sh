ca5da878fcbb46c6947f2ee74ededa1ec4366cbb
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-26 20:31:13 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 cd ../src
 # python train.py -m 'clf_efficientnet_b3' -c '...'
 # python train.py -m 'clf_resnest50' -c '...'
-# python train.py -m 'clf_resnet18' -c 'test kernel push'
+# python train.py -m 'clf_resnet18' -c 'test imgae_mask'
 # python train.py -m 'clf_resnet50' -c '...'
-python train.py -m 'clf_se_resnext101_32x4d' -c 'test kernel push'
+python train.py -m 'clf_se_resnext50_32x4d' -c 'test imgae_mask'
--- src/dataset/custom_dataset.py
+++ src/dataset/custom_dataset.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 from torch.utils.data import Dataset
@@ -24,14 +25,39 @@ class CustomDataset(Dataset):
         self.labels = labels
         self.transforms = get_transforms(self.cfg)
         self.is_train = cfg.is_train
-        self.image_path = '../data/input/train_images'
+        self.img_type = cfg.img_type
+        if self.img_type == 'image':
+            self.image_path = '../data/input/train_images'
+        elif self.img_type == 'mask':
+            self.mask_path = '../data/input/train_label_masks'
+        elif self.img_type == 'image_mask':
+            self.image_path = '../data/input/train_images'
+            self.mask_path = '../data/input/train_label_masks'
 
     def __len__(self):
         return len(self.image_ids)
 
     def __getitem__(self, idx):
         image_id = self.image_ids[idx]
-        image = cv2.imread(f'{self.image_path}/{image_id}.png')
+
+        if self.img_type == 'image':
+            image = cv2.imread(f'{self.image_path}/{image_id}.png')
+        elif self.img_type == 'mask':
+            image = cv2.imread(f'{self.mask_path}/{image_id}_mask.png')
+        elif self.img_type == 'image_mask':
+            raw_image = cv2.imread(f'{self.image_path}/{image_id}.png')
+            raw_image = 255 - raw_image
+            if os.path.exists(f'{self.mask_path}/{image_id}_mask.png'):
+                mask = cv2.imread(f'{self.mask_path}/{image_id}_mask.png')
+                mask = (mask[:, :, 0] /mask[:, :, 0].max()  * 255)
+
+                image = raw_image.copy()
+                image[:, :, 0] = image[:, :, 0] * 0.5 + mask * 0.5
+                image[:, :, 1] = image[:, :, 1] * 0.5 + mask * 0.5
+                image[:, :, 2] = image[:, :, 2] * 0.5 + mask * 0.5
+            else:
+                image = raw_image.copy()
+
         image = 255 - (image * (255.0/image.max())).astype(np.uint8)
         image = cv2.resize(image, dsize=(self.cfg.img_size.height, self.cfg.img_size.width))
         if self.transforms:
