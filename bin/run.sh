3dcfc4eb6242ce283a0f35ad805469315e5f240b
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-26 21:49:47 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -3,7 +3,7 @@ cd ../src
 # python train.py -m 'clf_resnest50' -c '...'
 # python train.py -m 'clf_resnet18' -c 'test imgae_mask'
 # python train.py -m 'clf_resnet50' -c '...'
-python train.py -m 'clf_se_resnext50_32x4d' -c 'img_size=312'
+python train.py -m 'clf_se_resnext50_32x4d' -c 'T_max=10'
 
 
 cd ../
