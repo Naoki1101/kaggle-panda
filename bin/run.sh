fe360bbff77380f3fa75c3f73cae8c7674404909
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-26 20:54:27 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -4,3 +4,9 @@ cd ../src
 # python train.py -m 'clf_resnet18' -c 'test imgae_mask'
 # python train.py -m 'clf_resnet50' -c '...'
 python train.py -m 'clf_se_resnext50_32x4d' -c 'test imgae_mask'
+
+
+cd ../
+git add -A
+git commit -m '...'
+git push origin master
