51a38654b5ba2d2b51ff45aa34475bb01c594cbd
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-27 20:44:08 +0900

    ...

--- src/loss.py
+++ src/loss.py
@@ -1,5 +1,9 @@
+import math
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.loss import _WeightedLoss
 
 
 # =============================================================================
@@ -33,3 +37,158 @@ class CrossEntropyLoss(nn.Module):
         
     def forward(self,yhat,y):
         return self.xloss(yhat,y)
+
+
+class SmoothCrossEntropyLoss(_WeightedLoss):
+    # https://stackoverflow.com/questions/55681502/label-smoothing-in-pytorch
+    def __init__(self, weight=None, reduction='mean', smoothing=0.0):
+        super().__init__(weight=weight, reduction=reduction)
+        self.smoothing = smoothing
+        self.weight = weight
+        self.reduction = reduction
+
+    @staticmethod
+    def _smooth_one_hot(targets:torch.Tensor, n_classes:int, smoothing=0.0):
+        assert 0 <= smoothing < 1
+        with torch.no_grad():
+            targets = torch.empty(size=(targets.size(0), n_classes),
+                    device=targets.device) \
+                .fill_(smoothing /(n_classes-1)) \
+                .scatter_(1, targets.data.unsqueeze(1), 1.-smoothing)
+        return targets
+
+    def forward(self, inputs, targets):
+        targets = SmoothCrossEntropyLoss._smooth_one_hot(targets, inputs.size(-1),
+            self.smoothing)
+        lsm = F.log_softmax(inputs, -1)
+
+        if self.weight is not None:
+            lsm = lsm * self.weight.unsqueeze(0)
+
+        loss = -(targets * lsm).sum(-1)
+
+        if  self.reduction == 'sum':
+            loss = loss.sum()
+        elif  self.reduction == 'mean':
+            loss = loss.mean()
+
+        return loss
+
+
+# https://kyudy.hatenablog.com/entry/2019/05/20/105526
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, eps=1e-7):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+        
+    def forward(self, input, target):
+        logit = F.softmax(input, dim=1)
+        logit = logit.clamp(self.eps, 1. - self.eps)
+        logit_ls = torch.log(logit)
+        loss = F.nll_loss(logit_ls, target, reduction="none")
+        view = target.size() + (1,)
+        index = target.view(*view)
+        loss = loss * (1 - logit.gather(1, index).squeeze(1)) ** self.gamma # focal loss
+
+        return loss.sum()
+
+
+# https://github.com/vandit15/Class-balanced-loss-pytorch/blob/master/class_balanced_loss.py
+def focal_loss(labels, logits, alpha, gamma):
+    """Compute the focal loss between `logits` and the ground truth `labels`.
+    Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
+    where pt is the probability of being classified to the true class.
+    pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
+    Args:
+      labels: A float tensor of size [batch, num_classes].
+      logits: A float tensor of size [batch, num_classes].
+      alpha: A float tensor of size [batch_size]
+        specifying per-example weight for balanced cross entropy.
+      gamma: A float scalar modulating loss from hard and easy examples.
+    Returns:
+      focal_loss: A float32 scalar representing normalized total loss.
+    """    
+    BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
+
+    if gamma == 0.0:
+        modulator = 1.0
+    else:
+        modulator = torch.exp(-gamma * labels * logits - gamma * torch.log(1 + 
+            torch.exp(-1.0 * logits)))
+
+    loss = modulator * BCLoss
+
+    weighted_loss = alpha * loss
+    focal_loss = torch.sum(weighted_loss)
+
+    focal_loss /= torch.sum(labels)
+    return focal_loss
+
+
+class ClassBalancedLoss(nn.Module):
+    def __init__(self, samples_per_cls, no_of_classes, loss_type, beta, gamma):
+        """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+        Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+        where Loss is one of the standard losses used for Neural Networks.
+        Args:
+        labels: A int tensor of size [batch].
+        logits: A float tensor of size [batch, no_of_classes].
+        samples_per_cls: A python list of size [no_of_classes].
+        no_of_classes: total number of classes. int
+        loss_type: string. One of "sigmoid", "focal", "softmax".
+        beta: float. Hyperparameter for Class balanced loss.
+        gamma: float. Hyperparameter for Focal loss.
+        Returns:
+        cb_loss: A float tensor representing class balanced loss
+        """
+        super(ClassBalancedLoss, self).__init__()
+        self.samples_per_cls = samples_per_cls
+        self.no_of_classes = no_of_classes
+        self.loss_type = loss_type
+        self.beta = beta
+        self.gamma = gamma
+
+    def forward(self, logits, labels):
+        effective_num = 1.0 - torch.pow(self.beta, self.samples_per_cls)
+        weights = (1.0 - self.beta) / torch.tensor(effective_num)
+        weights = weights / torch.sum(weights) * self.no_of_classes
+
+        labels_one_hot = F.one_hot(labels, self.no_of_classes).float()
+
+        weights = torch.tensor(weights).float()
+        weights = weights.unsqueeze(0)
+        weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
+        weights = weights.sum(1)
+        weights = weights.unsqueeze(1)
+        weights = weights.repeat(1,self.no_of_classes)
+
+        if self.loss_type == "focal":
+            cb_loss = focal_loss(labels_one_hot, logits, weights, self.gamma)
+        elif self.loss_type == "sigmoid":
+            cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
+        elif self.loss_type == "softmax":
+            pred = logits.softmax(dim = 1)
+            cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
+        return cb_loss
+
+
+class OhemLoss(nn.Module):
+    def __init__(self, rate):
+        super(OhemLoss, self).__init__()
+        self.rate = rate
+
+    def update_rate(self, rate):
+        self.rate = rate
+        
+    def forward(self, cls_pred, cls_target):
+        batch_size = cls_pred.size(0) 
+        ohem_cls_loss = F.cross_entropy(cls_pred, cls_target, reduction='none', ignore_index=-1)
+
+        sorted_ohem_loss, idx = torch.sort(ohem_cls_loss, descending=True)
+        keep_num = min(sorted_ohem_loss.size()[0], int(batch_size*self.rate) )
+        if keep_num < sorted_ohem_loss.size()[0]:
+            keep_idx_cuda = idx[:keep_num]
+            ohem_cls_loss = ohem_cls_loss[keep_idx_cuda]
+        cls_loss = ohem_cls_loss.sum() / keep_num
+        return cls_loss
