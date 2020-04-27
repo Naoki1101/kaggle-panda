e8609e8f36d11f02bf252d3a78509f4b11ed58dc
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-27 20:46:49 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 cd ../src
 # python train.py -m 'clf_efficientnet_b3' -c '...'
 # python train.py -m 'clf_resnest50' -c '...'
-python train.py -m 'clf_resnet18' -c 'test imgae_mask'
+python train.py -m 'clf_resnet18' -c 'test FocalLoss'
 # python train.py -m 'clf_resnet50' -c '...'
 # python train.py -m 'clf_se_resnext50_32x4d' -c 'stratify=data_provider'
 
