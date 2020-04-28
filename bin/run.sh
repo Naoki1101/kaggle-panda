be777b6a5a75a329ea27b884d91386094dc71243
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-04-28 11:49:27 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,9 @@
 cd ../src
 # python train.py -m 'clf_efficientnet_b3' -c '...'
-python train.py -m 'clf_resnest50' -c 'SmoothCrossEntropyLoss'
+# python train.py -m 'clf_resnest50' -c 'SmoothCrossEntropyLoss'
 # python train.py -m 'clf_resnet18' -c 'CustomSmoothCrossEntropyLoss'
 # python train.py -m 'clf_resnet50' -c '...'
-# python train.py -m 'clf_se_resnext50_32x4d' -c 'SmoothCrossEntropyLoss'
+python train.py -m 'clf_se_resnext50_32x4d' -c 'SmoothCrossEntropyLoss'
 
 # python train.py -m 'reg_resnet18' -c 'test regression'
 
