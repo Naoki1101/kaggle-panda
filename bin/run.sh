1eeea6e9322f0959e950b8d17d238ec5420b8d98
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-01 09:18:05 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -6,9 +6,9 @@ cd ../src
 # python train.py -m 'clf_se_resnext50_32x4d' -c 'SmoothCrossEntropyLoss'
 
 # python train.py -m 'reg_resnest50' -c '...'
-python train.py -m 'reg_resnet18' -c '...'
+# python train.py -m 'reg_resnet18' -c '...'
 # python train.py -m 'reg_resnet34' -c 'img_size=1280'
-# python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=620'
+python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=1280'
 
 cd ../
 git add -A
