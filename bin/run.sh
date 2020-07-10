7cdd2f5f0e89537c9e7b1dd052c78c71c78ac993
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-10 13:52:40 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -16,14 +16,14 @@ cd ../src
 # =============================================================================
 # Regression
 # =============================================================================
-python train.py -m 'reg_efficientnet_b0' -c 'test'
+# python train.py -m 'reg_efficientnet_b0' -c 'test'
 
 # python train.py -m 'reg_ghostnet' -c 'test'
 
 # python train.py -m 'reg_resnest50' -c '...'
 
 # python train.py -m 'reg_resnet18' -c 'test'
-# python train.py -m 'reg_resnet34' -c 'MSELoss'
+python train.py -m 'reg_resnet34' -c 'seed=2021'
 # python train.py -m 'reg_resnet50' -c 'img_size=c'
 
 # python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=768'
