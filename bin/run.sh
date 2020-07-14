e6ae56c9d6238ba240d3ed4ce05a62f236b5a2bf
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-14 23:12:19 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -23,7 +23,7 @@ cd ../src
 # python train.py -m 'reg_resnest50' -c '...'
 
 # python train.py -m 'reg_resnet18' -c 'test tta'
-python train.py -m 'reg_resnet34' -c 'noise_idx'
+python train.py -m 'reg_resnet34' -c 'tilex32'
 # python train.py -m 'reg_resnet50' -c 'img_size=c'
 
 # python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=768'
