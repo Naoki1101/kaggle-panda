a99ab53989283d47f18abb25582d11c4e8e6fb97
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-16 12:42:53 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -24,11 +24,12 @@ cd ../src
 # python train.py -m 'reg_resnest50' -c '...'
 
 # python train.py -m 'reg_resnet18' -c 'test tta'
-python train.py -m 'reg_resnet34' -c 'large_diff_idx'
-python train.py -m 'reg_resnet34_2' -c 'noise_idx'
-python train.py -m 'reg_resnet34_3' -c 'duplicate_img_idx, noise_idx'
-python train.py -m 'reg_resnet34_4' -c 'duplicate_img_idx, noise_idx, large_diff_idx'
-python train.py -m 'reg_resnet34_5' -c '128x144'
+# python train.py -m 'reg_resnet34' -c 'large_diff_idx'
+# python train.py -m 'reg_resnet34_2' -c 'noise_idx'
+# python train.py -m 'reg_resnet34_3' -c 'duplicate_img_idx, noise_idx'
+# python train.py -m 'reg_resnet34_4' -c 'duplicate_img_idx, noise_idx, large_diff_idx'
+# python train.py -m 'reg_resnet34_5' -c '128x144'
+python train.py -m 'reg_resnet34_6' -c '128x144, large_diff_idx'
 # python train.py -m 'reg_resnet50' -c 'img_size=c'
 
 # python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=768'
