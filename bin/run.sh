f3737eece41334b5b6ea783ee9e02a494be57e30
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-19 14:09:11 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -24,7 +24,7 @@ cd ../src
 # python train.py -m 'reg_resnest50' -c '...'
 
 # python train.py -m 'reg_resnet18' -c 'test tta'
-python train.py -m 'reg_resnet34' -c 'use top16 tiles'
+# python train.py -m 'reg_resnet34' -c 'use top16 tiles'
 # python train.py -m 'reg_resnet34_2' -c 'noise_idx'
 # python train.py -m 'reg_resnet34_3' -c 'duplicate_img_idx, noise_idx'
 # python train.py -m 'reg_resnet34_4' -c 'duplicate_img_idx, noise_idx, large_diff_idx'
@@ -43,7 +43,7 @@ python train.py -m 'reg_resnet34' -c 'use top16 tiles'
 
 # python train.py -m 'reg_resnet50' -c 'img_size=c'
 
-# python train.py -m 'reg_se_resnext50_32x4d' -c 'img_size=768'
+python train.py -m 'reg_se_resnext50_32x4d' -c 'use top9 tiles'
 
 
 # =============================================================================
